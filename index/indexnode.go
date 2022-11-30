678f9341a4936f50d27119df3379c14fb7fa8562
Pigeon377 <892197240@qq.com>
2022-11-30 17:57:32 +0800

    feat(index): add some field in indexnode / leafnode

--- index/indexnode.go
+++ index/indexnode.go
@@ -3,11 +3,15 @@ package index
 import "Duckweed/page"
 
 type IndexNode struct {
+	page     *page.Page
+	keys     []int // 键后续可能会扩展(多种类型) 但我要想先做个int的试试
+	children []int
 }
 
+// TODO 想出有哪些meta信息
+
 func (node *IndexNode) GetPage() *page.Page {
-	//TODO implement me
-	panic("implement me")
+	return node.page
 }
 
 func (node *IndexNode) ToBytes() []byte {
@@ -16,5 +20,8 @@ func (node *IndexNode) ToBytes() []byte {
 }
 
 func IndexNodeFromPage(page *page.Page) *IndexNode {
-	return nil
+	node := &IndexNode{
+		page: page,
+	}
+	return node
 }
--- index/leafnode.go
+++ index/leafnode.go
@@ -3,11 +3,14 @@ package index
 import "Duckweed/page"
 
 type LeafNode struct {
+	page         *page.Page
+	keys         []int // 键后续可能会扩展(多种类型) 但我要想先做个int的试试
+	rids         []string
+	rightSibling int
 }
 
 func (node *LeafNode) GetPage() *page.Page {
-	//TODO implement me
-	panic("implement me")
+	return node.page
 }
 
 func (node *LeafNode) ToBytes() []byte {
